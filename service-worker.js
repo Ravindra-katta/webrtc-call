2a3ee409e069615d6d5691c2eab75638668ff8e2
Katta <ravindra.katta1027@gmail.com>
2025-04-16 16:33:46 +0530

    Replaced old files with new project files

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,16 @@
-self.addEventListener("install", event => {
-  console.log("Service worker installed");
-  self.skipWaiting();
+// service-worker.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker Installed');
 });
 
-self.addEventListener("activate", event => {
-  console.log("Service worker activated");
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker Activated');
 });
 
-self.addEventListener("fetch", event => {
-  // Optionally cache resources or handle offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
